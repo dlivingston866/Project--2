d5c7821e6d5ca15aaaf79f0f02f3b6dedbbe160a
Trevor McNamara <trevormcnamara@email.arizona.edu>
2020-08-29 08:45:33 -0700

    new schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,23 @@
 DROP DATABASE IF EXISTS new_foodsDB;
 CREATE database new_foodsDB;
+USE new_foodsDB;
+
+CREATE TABLE newfoods (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (255) NULL,
+    category VARCHAR (255) NOT NULL,
+    cost INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+LOAD DATA INFILE 'c:/tmp/foods.csv'
+INTO TABLE newfoods
+FIELDS TERMINATED BY ','
+ENCLOSED BY ' " '
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+
 
 
